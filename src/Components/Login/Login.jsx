1f1108b0aaddb71dacdb9471de155fcc3a424b76
Thomas Campanholi <thomasaqw@live.com>
2022-02-14 10:19:26 -0300

    first commit

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+    return (
+        <div className='container'>
+            <div className='container-div'>
+                <div className='formsDiv'>
+                    <span>Login</span>
+                    <div className='form'>
+                        <input className='login-input' type="text" placeholder='e-mail' />
+                        <span className='form-border'></span>
+                    </div>
+                    <div className='form'>
+                        <input className='login-input' type="text" placeholder='senha' />
+                        <span className='form-border'></span>
+                    </div>
+                </div>
+                <div className='loginDiv'>
+                    <button>Login</button>
+                    <a href="#">Esqueceu a senha?</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
